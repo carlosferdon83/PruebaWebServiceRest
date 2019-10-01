fdaab1d27e39fc4ab5eea9c6d66573e7058dbe32
Carlos Fernando <Carlos Fernando@DESKTOP-L377F63>
2019-10-01 01:41:38 -0500

    Redistribucion de paquetes.

--- src/main/java/com/rest/ws/dao/service/CompanyFacadeLocal.java
+++ src/main/java/com/rest/ws/dao/service/CompanyFacadeLocal.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.rest.ws.dao.service;
 
 import com.rest.ws.model.entities.Company;
 import java.util.List;
 import javax.ejb.Local;
 
-/**
- *
- * @author Carlos Fernando
- */
 @Local
 public interface CompanyFacadeLocal {
 
@@ -29,5 +20,4 @@ public interface CompanyFacadeLocal {
     List<Company> findRange(int[] range);
 
     int count();
-    
 }
